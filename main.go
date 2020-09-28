08bddcbafdb1a1e79eda972ed4b47bcaace0d785
Alexis Guzmán <alegu@Alexiss-MacBook-Pro.local>
2020-09-28 08:12:52 -0500

    start e at 0

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var e float64
+	var e float64 = 0
 	var lim int
 
 	fmt.Scan(&lim)
@@ -13,7 +13,7 @@ func main() {
 		for j := i; j > 0; j-- {
 			fact *= float64(j)
 		}
-		e = e + (1.0 / fact)
+		e += (1.0 / fact)
 	}
 	fmt.Print(e)
 }
